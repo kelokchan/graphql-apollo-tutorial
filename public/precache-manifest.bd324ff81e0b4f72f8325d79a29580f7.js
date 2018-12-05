61580674ccb59dbe7c7a1763a2ccf77b8acca593
Kelok Chan <kelokchan@gmail.com>
2018-12-05 16:51:49 +0800

    Initial

---
+++ public/precache-manifest.bd324ff81e0b4f72f8325d79a29580f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "37a39b35e699355acde6016e5ce69039",
+    "url": "/static/media/logo.37a39b35.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "65ff95cedc7833252b83",
+    "url": "/static/js/main.65ff95ce.chunk.js"
+  },
+  {
+    "revision": "015c8e26dbfd446df244",
+    "url": "/static/js/1.015c8e26.chunk.js"
+  },
+  {
+    "revision": "65ff95cedc7833252b83",
+    "url": "/static/css/main.0f72f137.chunk.css"
+  },
+  {
+    "revision": "015c8e26dbfd446df244",
+    "url": "/static/css/1.978503a7.chunk.css"
+  },
+  {
+    "revision": "9025303b166e701f87db9b985d544d3d",
+    "url": "/index.html"
+  }
+];
